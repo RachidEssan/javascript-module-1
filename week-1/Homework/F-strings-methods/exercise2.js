f80c7f9e68815cd0b69818fa2f4e403e307f70e2
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-04-20 19:57:37 +0200

    Exercise F done

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,4 +1,4 @@
-const familyname = "Essantouh";
-let message=familyname.trim();
+const familyname = "   Essantouh    ";
+let message = familyname.trim();
 
 console.log(`My family name is ${message} and it's is 9 characters long`);
